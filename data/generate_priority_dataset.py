7c6648e82ab00210911aa7828b2e58ee0d3588f3
Gustavo Fernandez <gustavo.fernandez.costa@gmail.com>
2021-07-01 17:16:40 -0300

    Priority dataset and client writes to file

---
+++ data/generate_priority_dataset.py
@@ -0,0 +1,26 @@
+import csv
+
+with open('example.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    new_csv = []
+    for row in csv_reader:
+        new_row = []
+        i = 0
+        if line_count > 0:
+            for element in row:
+                if i == 0:
+                    new_row.append(element)
+                else:
+                    level = int(int(element)/25)
+                    new_row.append((level, int(element)))
+                i += 1
+            new_csv.append(new_row)
+
+        line_count += 1
+
+    with open('priority_example.csv', 'w') as f:
+        writer = csv.writer(f)
+
+        # write multiple rows
+        writer.writerows(new_csv)
--- src/client.py
+++ src/client.py
@@ -1,10 +1,11 @@
 import asyncio
 import csv
 
-from aioconsole import ainput
 from aioquic.asyncio import QuicConnectionProtocol
 from aioquic.asyncio.client import connect
 from aioquic.quic.configuration import QuicConfiguration
+
+CLIENT_ID = '1'
 
 async def aioquic_client():
     configuration = QuicConfiguration(is_client=True)
@@ -18,22 +19,22 @@ async def handle_stream(reader, writer):
     # User input
     asyncio.ensure_future(receive(reader))
     # Server data received
-    with open('../data/example.csv') as csv_file:
+    writer.write(CLIENT_ID.encode())
+    await asyncio.sleep(1)
+    with open('../data/priority_example.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
         for row in csv_reader:
-            if line_count > 0:
-                message = ''.join(row)
-                print("Sending: " + message)
-                writer.write(message.encode())
-                await asyncio.sleep(1)
-            line_count += 1
+            message = ''.join(row)
+            print("Sending: " + message)
+            writer.write(message.encode())
+            await asyncio.sleep(1)
 
 async def receive(reader):
     while True:
         input_message = await reader.read(1024)
-        print('print incoming message', input_message)
-
+        f = open("../logs/client_received.txt", "a")
+        f.write(input_message.decode() + '\n')
+        f.close()
 
 async def main():
     await aioquic_client()
