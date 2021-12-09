c21a38ebc021db2e5485c940141715f54b0f4d0a
Gustavo Fernandez da Costa <gustavo.fernandez.costa@gmail.com>
2021-12-09 14:17:03 -0300

    Adding python path

--- mininet_config.py
+++ mininet_config.py
@@ -58,9 +58,11 @@ def launch():
     server = hosts[0]
     client = hosts[1]
 
+    server.cmd("export PYTHONPATH=$PYTHONPATH:/root/aioquic-360-video-streaming/src")
     server.cmd("nohup python3 src/server.py -c 'cert/ssl_cert.pem' -k 'cert/ssl_key.pem' -q 'WFQ' >> server_out.txt &")
     #server.cmd("nohup dist/server/server -c 'cert/ssl_cert.pem' -k 'cert/ssl_key.pem' >> server_out.txt &")
 
+    client.cmd("export PYTHONPATH=$PYTHONPATH:/root/aioquic-360-video-streaming/src")
     client.cmd("python3 src/client.py -c cert/pycacert.pem 127.0.0.1:4433 -i data/user_input.csv >> client_out.txt")
     #client.cmd("dist/client/client -c 'cert/pycacert.pem' -i 'data/user_input.csv' -u 'wss://10.0.0.1:4433' -da basic2 >> client_out.txt")
 
